ae6fe4de613faa0856277f2590387178fa1b7071
zsp108 <echo996@foxmail.com>
2025-02-13 17:13:25 +0800

    fix(gitinstall): fix git install scrpits lib

--- scripts/git_install.sh
+++ scripts/git_install.sh
@@ -33,34 +33,11 @@ function log {
 git_version="2.42.0"
 
 
-#判断是否安装过git，如果有再确认是否卸载原安装的git
-if [ `command  -v git` ];then
-    cur_gitversion=`git --version 2>&1 | sed '1!d' | sed -e 's/"//g' | awk '{print $3}'`
-    log info "当前git版本为$cur_gitversion"
-    if [[ $cur_gitversion == $git_version  ]];then
-        log info "已安装的版本和将要安装的版本相同,不进行安装"
-        return
-    else
-        log info "已安装的版本和将要安装的版本不同"
-        read -p "是否删除已安装的git$cur_gitversion？(y/n):" is_del_git
 
-        if [[ $is_del_git == 'y' ]];then
-            case $OSNAME in
-                'redhat')
-                    yum remove git -y
-                    yum -y update && \
-                    yum -y install epel-release && \
-                    yum install -y wget make autoconf automake cmake perl-CPAN libcurl-devel libtool gcc gcc-c++ glibc-headers zlib-devel telnet ctags lrzsz jq expat-devel openssl-devel gettext openssh-server passwd
-                    ;;
-                'debian')
-                    apt remove git -y
-                    ;;
-            esac
-        fi
-    fi
-
-fi
-
+# 打印欢迎信息
+log info "欢迎使用 Git 安装脚本！"
+log info "此脚本将会安装 Git $git_version。"
+log info "正在执行安装操作，请耐心等待..."
 
 # 获取 CPU 架构
 ARCH=$(uname -m)
@@ -78,6 +55,45 @@ fi
 # 打印系统信息
 log info "检测到系统架构: $ARCH"
 log info "检测到操作系统: $OS $VERSION"
+
+#判断是否安装过git，如果有再确认是否卸载原安装的git
+if [ `command  -v git` ];then
+    cur_gitversion=`git --version 2>&1 | sed '1!d' | sed -e 's/"//g' | awk '{print $3}'`
+    log info "当前git版本为$cur_gitversion"
+    if [[ $cur_gitversion == $git_version  ]];then
+        log info "已安装的版本和将要安装的版本相同,不进行安装"
+        return
+    else
+        log info "已安装的版本和将要安装的版本不同"
+        read -p "是否删除已安装的git$cur_gitversion？(y/n):" is_del_git
+
+        if [[ $is_del_git == 'y' ]];then
+            case $OS in
+                'centos' | 'rhel')
+                    yum remove git -y
+                    ;;
+                'debian' | 'ubuntu')
+                    apt remove git -y
+                    ;;
+            esac
+        fi
+    fi
+
+fi
+
+# 判断是否为 CentOS/RedHat 系统
+if [[ $OS == 'centos' || $OS == 'rhel' ]]; then 
+    yum -y update && \
+    yum -y install epel-release && \
+    yum install -y wget make autoconf automake cmake perl-CPAN libcurl-devel libtool gcc gcc-c++ glibc-headzlib-devel telnet ctags lrzsz jq expat-devel openssl-devel gettext openssh-server passwd
+# 判断是否为 Ubuntu/Debian 系统
+elif [[ $OS == 'ubuntu' || $OS == 'debian' ]]; then
+    apt-get update && \
+    apt-get install -y wget make autoconf automake cmake perl libwww-perl libcurl4-openssl-dev libtool gcc g++ glibc-doc telnet ctags lrzsz jq expat libexpat1-dev openssl libssl-dev gettext openssh-server passwd
+else
+    log error "不支持的操作系统类型。"
+    # exit 1
+fi
 
 # 下载 Git 安装包
 log info "正在下载 Git 安装包..."
