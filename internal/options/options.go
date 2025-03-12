e61763195693c0f980216ea0a6138c485f2c957e
zsp108 <echo996@foxmail.com>
2025-03-12 16:56:01 +0800

    perf(options): add options default values

--- internal/options/options.go
+++ internal/options/options.go
@@ -1,6 +1,11 @@
 package options
 
-import "time"
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/zsp108/dbbmsql/pkg/logger"
+)
 
 type Options struct {
 	GeneralOptions   GeneralOptions   `yaml:"general"`
@@ -9,8 +14,9 @@ type Options struct {
 }
 
 type GeneralOptions struct {
-	LogDir   string `yaml:"logdir" default:"dbbmsql.log"`
-	LogLevel string `yaml:"loglevel" default:"info"`
+	LogOutPut string `yaml:"logoutput" default:"dbbmsql.log"`
+	LogLevel  string `yaml:"loglevel" default:"info"`
+	LogType   string `yaml:"logtype" default:"text"`
 }
 
 type MYSQLOptions struct {
@@ -33,4 +39,42 @@ type OceanbaseOptions struct {
 	MaxIdleConnections    int           `yaml:"max_idle_connections"`
 	MaxOpenConnections    int           `yaml:"max_open_connections"`
 	MaxConnectionLifeTime time.Duration `yaml:"max_connection_life_time"`
+}
+
+// 构造函数，设置日志默认值
+func NewLoggerOptions() *GeneralOptions {
+	return &GeneralOptions{
+		LogOutPut: "stdout", // 默认值
+		LogLevel:  "info",   // 默认值
+		LogType:   "text",   // 默认值
+	}
+}
+
+// 构造函数，设置Mysql默认值
+func NewMYSQLOptions() *MYSQLOptions {
+	return &MYSQLOptions{
+		MaxIdleConnections:    10,            // 默认值
+		MaxOpenConnections:    100,           // 默认值
+		MaxConnectionLifeTime: 1 * time.Hour, // 默认值
+	}
+}
+
+// 构造函数，设置OB默认值
+func NewOceanbaseOptions() *MYSQLOptions {
+	return &MYSQLOptions{
+		MaxIdleConnections:    10,            // 默认值
+		MaxOpenConnections:    100,           // 默认值
+		MaxConnectionLifeTime: 1 * time.Hour, // 默认值
+	}
+}
+
+func NewLogger(opts *Options) *logger.Logger {
+
+	level, err := logrus.ParseLevel(opts.GeneralOptions.LogLevel)
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+	logger := logger.NewLogger(level, opts.GeneralOptions.LogType, opts.GeneralOptions.LogOutPut)
+
+	return logger
 }
