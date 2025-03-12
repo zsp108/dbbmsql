fcf5f9420b2b2ec28f7af9d515eea16ce4f4d055
zsp108 <echo996@foxmail.com>
2025-03-12 17:14:04 +0800

    perf(flags): optimization-h and-c can not be used together

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -16,7 +16,6 @@ type FlagStruct struct {
 }
 
 func GetFlages() (*FlagStruct, error) {
-	// TODO: add flags here
 	helpFlagLong := flag.Bool("help", false, "Show help message")
 	hostFlag := flag.String("h", "", "DB Host")
 	cnfigFlag := flag.String("c", "", "Config file path")
@@ -31,24 +30,32 @@ func GetFlages() (*FlagStruct, error) {
 		fmt.Println("  -c, --config string  Config file path")
 		fmt.Println("  -u, --user string    DB User")
 		fmt.Println("  -p, --pwd string     DB Password")
-		fmt.Println("  -P, --port int       DB Port")
+		fmt.Println("  -P, --port int       DB Port (default: 3306)")
 		fmt.Println("  -d, --db string      DB Name")
 	}
 
 	flag.Parse()
-	// 如果用户没有输入任何参数或者输入 -h 或 --help，显示帮助信息并退出
+
+	// 如果用户没有输入任何参数或者输入 -help，显示帮助信息并退出
 	if flag.NFlag() == 0 || *helpFlagLong {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	flagstr := FlagStruct{}
+	// 规则 1：-c 和 -h 不能同时提供
+	if *cnfigFlag != "" && *hostFlag != "" {
+		return nil, fmt.Errorf("cannot use -c (config) and -h (host) at the same time")
+	}
+
+	// 规则 2：如果提供了 -h，则必须提供 -u、-p、-d
 	if *hostFlag != "" {
-		if *userFlag == "" || *pwdFlag == "" || *portFlag == 0 || *dbFlag == "" {
-			return nil, fmt.Errorf("if hostFlag is provided, user, password, port, and dbname must also be provided")
+		if *userFlag == "" || *pwdFlag == "" || *dbFlag == "" {
+			return nil, fmt.Errorf("when using -h (host), you must also provide -u (user), -p (password), and -d (dbname)")
 		}
 	}
 
+	// 组装 FlagStruct
+	flagstr := FlagStruct{}
 	if *cnfigFlag != "" {
 		flagstr.CnfigFlag = *cnfigFlag
 	} else {
